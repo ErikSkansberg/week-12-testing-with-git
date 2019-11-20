3718f19ef5770cdbc5fc45feb2cedfafcd8f7b1d
Erik <Erik@doitbetters.com>
2019-11-20 13:41:22 -0700

    update to the first two functions

--- week 11/week_11.py
+++ week 11/week_11.py
@@ -1,2 +1,9 @@
 #testing things in py 
-
+def PrintOutput(string):
+  print("OUTPUT "+str(string))
+def LoadFile(filename):
+    Fin = open(filename,'r')
+    Line = []
+    for line in Fin:
+        Line.append(''.join(line))
+    print(Line)
