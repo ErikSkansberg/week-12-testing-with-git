8eb5340249e46c6637168fc04cb5ec7056c57f9f
Erik <Erik@doitbetters.com>
2019-11-20 20:59:09 -0700

    added findwords and scorefinder

--- week 11/week_11.py
+++ week 11/week_11.py
@@ -6,8 +6,30 @@ def LoadFile(filename):
     Line = []
     for line in Fin:
         Line.append(''.join(line))
+    Fin.close()
     PrintOutput(Line)
 def UpdateString(str1,str2,num):
     word1 = list(str1)
     word1[num] = str2
-    print(word1)
+    print("".join(word1))
+def FindWordCount(looking,fin):
+    count = 0
+
+    with open((file), 'r') as file:
+        for row in file:
+            for word in row.split():
+                if word == n:
+                    count = count + 1
+    return PrintOutput(count)
+def ScoreFinder(a,b,c):
+    count = 0
+    C = c
+    A = [x.upper() for x in a]
+    c = c.upper()
+    for i in A:
+        if i == c:
+            print("OUTPUT", C, "scored", b[A.index(c)])
+            count = 1
+
+    if count == 0:
+        print("OUTPUT Could not find Player")
